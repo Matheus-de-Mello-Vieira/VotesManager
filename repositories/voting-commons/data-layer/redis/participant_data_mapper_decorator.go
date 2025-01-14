1a45ae38139850d233ee4556645ff8c79eed197f
Matheus de Mello Vieira <mellomatheus67@gmail.com>
2025-01-14 06:41:32 -0300

    refactored code

--- repositories/voting-commons/data-layer/redis/participant_data_mapper_decorator.go
+++ repositories/voting-commons/data-layer/redis/participant_data_mapper_decorator.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"fmt"
 	"strconv"
-	"strings"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -60,66 +59,43 @@ func (mapper ParticipantDataMapperRedisDecorator) getGeneralTotal(ctx context.Co
 	return &result, err
 }
 func (mapper ParticipantDataMapperRedisDecorator) getVotesByParticipant(ctx context.Context) (map[domain.Participant]int, error) {
-	participantById, err := mapper.getParticipantById(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	var currentCursor uint64
-	results := map[domain.Participant]int{}
-
-	for {
-		keys, nextCursor, err := mapper.redis.Scan(ctx, currentCursor, "count:participant:*", 10).Result()
-		if err != nil {
-			return nil, err
-		}
-
-		mapper.appendResultsFromRedisKeys(ctx, participantById, results, keys)
-
-		currentCursor = nextCursor
-		if currentCursor == 0 {
-			break
-		}
-	}
-
-	return results, nil
-}
-func (mapper ParticipantDataMapperRedisDecorator) appendResultsFromRedisKeys(ctx context.Context, participantById map[int]domain.Participant, result map[domain.Participant]int, keys []string) error {
-	for _, key := range keys {
-		participantIDStr := strings.TrimPrefix(key, "count:participant:")
-		participantID, err := strconv.Atoi(participantIDStr)
-		if err != nil {
-			return err
-		}
-
-		countStr, err := mapper.redis.Get(ctx, key).Result()
-		if err != nil {
-			return err
-		}
-		count, err := strconv.Atoi(countStr)
-		if err != nil {
-			return err
-		}
-
-		result[participantById[participantID]] = count
-	}
-
-	return nil
-}
-
-func (mapper ParticipantDataMapperRedisDecorator) getParticipantById(ctx context.Context) (map[int]domain.Participant, error) {
-	result := map[int]domain.Participant{}
-
 	participants, err := mapper.FindAll(ctx)
 	if err != nil {
 		return nil, err
 	}
 
+	result := map[domain.Participant]int{}
 	for _, participant := range participants {
-		result[participant.ParticipantID] = participant
+		count, err := mapper.getCountOfParticipant(ctx, participant.ParticipantID)
+
+		if err != nil {
+			return nil, err
+		}
+
+		result[participant] = *count
 	}
+
 	return result, nil
 }
+func (mapper ParticipantDataMapperRedisDecorator) getCountOfParticipant(ctx context.Context, participantId int) (*int, error) {
+	key := fmt.Sprint("count:participant:", participantId)
+
+	countStr, err := mapper.redis.Get(ctx, key).Result()
+	if err != nil {
+		if err == redis.Nil { // don't exists
+			count := 0
+			return &count, nil
+		}
+		return nil, err
+	}
+	count, err := strconv.Atoi(countStr)
+	if err != nil {
+		return nil, err
+	}
+
+	return &count, nil
+}
+
 func (mapper ParticipantDataMapperRedisDecorator) getVotesByHour(ctx context.Context) ([]domain.TotalByHour, error) {
 	results, err := mapper.redis.HGetAll(ctx, "votes:by:hour").Result()
 	if err != nil {
